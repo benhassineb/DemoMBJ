180234e6c4fabd93a5a7ec97a55ff327a04f807b
bbh <b.benhassine@hotmail.com>
2023-05-11 23:03:05 +0200

    intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,15 @@ import { Chat } from '../components/Chat'
 function Home() {
   return (
     <Page className="flex flex-col gap-12">
-         <>
-              <p> Cette IA a utilisé uniquement les informations présentées dans ces deux pages web pour acquérir autant de connaissances que possible sur L'aide MOBILI-JEUNE :</p>
-
-              <ul>
-                <li><a href="https://mobilijeune.actionlogement.fr/faq">https://mobilijeune.actionlogement.fr/faq</a></li>
-                <li><a href="https://groupe-reussite.fr/ressources/financement-etudes-logement-aide-mobili-jeune/">https://groupe-reussite.fr/ressources/financement-etudes-logement-aide-mobili-jeune/</a></li>
-              </ul>
-              <p>En enrichissant les données fournies et en les rendant plus précises et plus pertinentes, on peut améliorer significativement la qualité des réponses.</p>
-
-            </>
+      <text>
+        Cette IA a utilisé uniquement les informations présentées dans ces deux pages web pour acquérir autant de connaissances que possible sur L'aide MOBILI-JEUNE : <br></br>
+        <br></br>
+        https://mobilijeune.actionlogement.fr/faq<br></br>
+        https://www.actionlogement.fr/l-aide-mobili-jeune<br></br>
+        <br></br>
+      
+        <b>En enrichissant les données fournies et en les rendant plus précises et plus pertinentes, on peut améliorer significativement la qualité des réponses.</b>
+      </text>
 
       <section className="flex flex-col gap-3">
         <Text variant="h2">ChatBot MOBILI-JEUNE®:</Text>
